06588e228682dcae6c3be622d206455ad7b238e0
fedEnder <lemmer.j.christopher@gmail.com>
2020-04-12 23:37:39 -0500

    New Web Project w/ Gulp (sass, autoprefix, watch)

--- index.php
+++ index.php
@@ -1,46 +1,15 @@
 <?php
-$options = array (
-    1 => array (
-        "move" => array (
-            1 => "N",
-            2 => "NE",
-            3 => "E",
-            4 => "SE",
-            5 => "S",
-            6 => "SW",
-            7 => "W",
-            8 => "NW"
-        )
-    ),
-    2 => array (
-        "attack" => array (
-            1 => "fast",
-            2 => "regular",
-            3 => "heavy"
-            
-        ),
-        "direction" => array (
-            1 => "N",
-            2 => "NE",
-            3 => "E",
-            4 => "SE",
-            5 => "S",
-            6 => "SW",
-            7 => "W",
-            8 => "NW"
-        ) 
-    )
-)
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap 4 Website Example</title>
+  <title>New Website | Homepage</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
